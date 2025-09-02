aa20c096d88d929d8ade81f03650e0cde278cc74
Durga-Jaiswal <jaiswaldurga555@gmail.com>
2025-09-02 23:55:49 +0530

    add custom api response and error

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        error = [],
+        message = "Something went wrong",
+        stack = "",
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.success = false
+        this.message = message
+        this.errors = error
+    
+
+    if(stack) {
+        this.stack = stack
+    } else {
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+}
+
+export { ApiError }
